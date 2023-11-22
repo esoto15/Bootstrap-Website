3ae2518a850f071c88f4adc41156f387ff658007
Elizabeth <esoto15@neiu.edu>
2023-11-22 00:46:07 -0600

    Cleaned up template

--- js/scripts.js
+++ js/scripts.js
@@ -40,12 +40,18 @@ window.addEventListener('DOMContentLoaded', event => {
 
 });
 
-/*multi item carousel*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
-/*
-    Carousel
-*/
-
-/*multi item carousel ends*/
-
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
 
